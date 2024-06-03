f4acf256e2701a447580f167fa151ee749977d53
Diki Wahyudi <whydiki2308@gmail.com>
2024-06-03 14:54:24 +0700

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,8 +17,11 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Beranda'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Fasum Diki'),
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
         actions: [
           IconButton(
             onPressed: () {
@@ -29,7 +32,8 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('posts').orderBy('timestamp', descending: true).snapshots(),
+        stream: FirebaseFirestore.instance.collection('posts').orderBy(
+            'timestamp', descending: true).snapshots(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -37,48 +41,75 @@ class HomeScreen extends StatelessWidget {
           if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
             return const Center(child: Text('Tidak ada postingan tersedia'));
           }
-          return ListView.builder(
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // Jumlah kolom
+              childAspectRatio: 1, // Rasio aspek untuk membuat gambar kotak
+            ),
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) {
               var post = snapshot.data!.docs[index];
               var data = post.data() as Map<String, dynamic>;
               var postTime = data['timestamp'] as Timestamp;
               var date = postTime.toDate();
-              var formattedDate = '${date.day}/${date.month}/${date.year} ${date.hour}:${date.minute}';
+              var formattedDate = '${date.day}/${date.month}/${date.year} ${date
+                  .hour}:${date.minute}';
 
-              // var username = data.containsKey('username') ? data['username'] : 'Anonim';
-              var imageUrl = data.containsKey('image_url') ? data['image_url'] : null;
+              var username = data.containsKey('username')
+                  ? data['username']
+                  : 'Anonim';
+              var imageUrl = data.containsKey('image_url')
+                  ? data['image_url']
+                  : null;
               var text = data.containsKey('text') ? data['text'] : '';
 
               return Card(
-                margin: EdgeInsets.all(10.0),
+                margin: EdgeInsets.all(4.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     if (imageUrl != null)
-                      Image.network(imageUrl),
+                      Expanded(
+                        child: Image.network(
+                          imageUrl,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: double.infinity,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Center(child: Text('Gagal memuat gambar'));
+                          },
+                        ),
+                      )
+                    else
+                      Expanded(
+                        child: Center(child: Text('Gambar tidak tersedia')),
+                      ),
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(4.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          // Text(
-                          //   username,
-                          //   style: TextStyle(
-                          //     fontWeight: FontWeight.bold,
-                          //     fontSize: 16,
-                          //   ),
-                          // ),
-                          // SizedBox(height: 4),
+                          Text(
+                            username,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                          ),
+                          SizedBox(height: 2),
                           Text(
                             formattedDate,
                             style: TextStyle(
                               color: Colors.grey,
-                              fontSize: 12,
+                              fontSize: 10,
                             ),
                           ),
-                          SizedBox(height: 8),
-                          Text(text),
+                          SizedBox(height: 4),
+                          Text(
+                            text,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ],
                       ),
                     ),
